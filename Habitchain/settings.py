ed65271c3a4df820eac0fd9b7203c11e91e26ea7
kidusking-glich <abelyohannes69@gmail.com>
2026-01-04 23:51:21 +0300

    Removing the admin styling package

--- Habitchain/settings.py
+++ Habitchain/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_spectacular',
-    'django_admin_bootstrap5',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',

758552e0890e9b4fd1de2135857783ff3da3e8da
kidusking-glich <abelyohannes69@gmail.com>
2026-01-04 23:02:59 +0300

    Updated settings.py with the correct Railway public connection string

--- Habitchain/settings.py
+++ Habitchain/settings.py
@@ -133,11 +133,11 @@ WSGI_APPLICATION = 'Habitchain.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME', 'railway'),
-        'USER': os.environ.get('DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'yfSFjyJNlMDwDNKUWQXjmdoznJNGAKjM'),
-        'HOST': os.environ.get('DB_HOST', 'mysql.railway.internal'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'yfSFjyJNlMDwDNKUWQXjmdoznJNGAKjM',
+        'HOST': 'yamabiko.proxy.rlwy.net',
+        'PORT': '54583',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
